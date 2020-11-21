f673dddfd2385dba0f87a3de22b87da6df821894
Chris <krzysiek9k@gmail.com>
2020-11-21 17:43:20 +0100

    Implemented highscore logic

--- script.js
+++ script.js
@@ -9,24 +9,51 @@
 // console.log(document.querySelector('.guess').value);
 
 const btn = document.querySelector('.check');
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+const btnAgain = document.querySelector('.again');
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
-document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.score').textContent = 20;
+
+console.log(secretNumber);
+
+btnAgain.addEventListener('click', () => {
+  score = 20;
+  document.querySelector('.score').textContent = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  console.log(secretNumber);
+});
 
 btn.addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
-
   if (score > 1) {
+    //When there is no input
     if (!guess) {
       document.querySelector('.message').textContent = '⛔ No number';
-    } else if (guess === secretNumber) {
+    }
+    //When player guess number
+    else if (guess === secretNumber) {
       document.querySelector('.message').textContent = '🍕 You won!';
-    } else if (guess > secretNumber) {
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').textContent = secretNumber;
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
+    }
+    //When guess is too high
+    else if (guess > secretNumber) {
       score--;
       document.querySelector('.message').textContent = '😫 Too high';
       document.querySelector('.score').textContent = score;
-    } else {
+    }
+    //When guess is too low
+    else {
       score--;
       document.querySelector('.message').textContent = '😫 Too low';
       document.querySelector('.score').textContent = score;
